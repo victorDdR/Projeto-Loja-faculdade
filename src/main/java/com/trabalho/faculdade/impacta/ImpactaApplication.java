d21d9c475123955a87e63e6e021923f689e74314
Luc4sC <lucasoull@gmail.com>
2026-02-21 10:03:16 -0300

    first commit

---
+++ src/main/java/com/trabalho/faculdade/impacta/ImpactaApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho.faculdade.impacta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpactaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpactaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trabalho/faculdade/impacta/ImpactaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trabalho.faculdade.impacta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpactaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
