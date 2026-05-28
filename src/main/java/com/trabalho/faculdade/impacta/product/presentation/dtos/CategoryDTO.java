73fcb3caef9a1527027c0b791ac5cc13a067e053
Luc4sC <lucasoull@gmail.com>
2026-05-28 08:54:03 -0300

    wip refatorando produto

--- src/main/java/com/trabalho/faculdade/impacta/product/presentation/dtos/CategoryDTO.java
+++ src/main/java/com/trabalho/faculdade/impacta/product/presentation/dtos/CategoryDTO.java
@@ -2,8 +2,9 @@ package com.trabalho.faculdade.impacta.product.presentation.dtos;
 
 import com.trabalho.faculdade.impacta.product.domain.Category;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-public record CategoryDTO(Long id, @NotBlank String name) {
+public record CategoryDTO(Long id, @NotBlank @Size(min = 3, max = 20) String name) {
 
     public CategoryDTO(Category category) {
         this(category.getId(), category.getName());
--- src/main/java/com/trabalho/faculdade/impacta/product/presentation/dtos/ProductDTO.java
+++ src/main/java/com/trabalho/faculdade/impacta/product/presentation/dtos/ProductDTO.java
@@ -1,17 +1,17 @@
 package com.trabalho.faculdade.impacta.product.presentation.dtos;
 
 import com.trabalho.faculdade.impacta.product.domain.Product;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 
 import java.math.BigDecimal;
 
 public record ProductDTO(Long id,
-                         @NotBlank
+                         @NotBlank @Size(min = 5, max = 25)
                          String productName,
                          @NotNull
+                         @Digits(integer = 1, fraction = 2) @DecimalMin(value = "5.00") @DecimalMax(value = "99999.99")
                          BigDecimal price,
-                         @NotNull
+                         @NotNull @Positive @Max(value = 9999)
                          Integer stock,
                          @NotNull
                          CategoryDTO category) {
